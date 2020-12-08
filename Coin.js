f82bfc0c7815974ee5ecba3f3d8c631b705430dd
hywepp <75666616+hywepp@users.noreply.github.com>
2020-12-08 17:56:34 +0900

    Add files via upload

---
+++ Coin.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// xrp, 리플이라는 코인이 있는데, xrp
+
+const Coin = mongoose.model("Coin", {
+  code: String,
+  active: Boolean
+});
+
+// xrp, btc, bch, eth, ech, xem..
+
+module.exports = {
+  Coin
+};
+
+// signup
+// signin
